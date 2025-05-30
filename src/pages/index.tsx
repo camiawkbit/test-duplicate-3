ef97ce47f22a11dd484416e3e71f9c532d6e8327
test <test@test.com>
2025-05-30 12:58:27 +0000

    Update site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
+import { Basic } from "@/components/Basic";
 export default function Page() {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <></>
+      <Basic t1="Esto es un bloque basico" />
     </>
   );
 }
